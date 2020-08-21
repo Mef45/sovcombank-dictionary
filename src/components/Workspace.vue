80f5e4aeaf54d9abe0581cc9a687d1a3d7000d13
mef45 <mefka45@yandex.ru>
2020-08-21 13:31:26 +0500

    WIP: реализован UI компонент для ввода текста

---
+++ src/components/Workspace.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="workspace">
+    </div>
+</template>
+
+<script lang="ts">
+    import axios from 'axios'
+
+    import { Component, Prop, Vue } from 'vue-property-decorator'
+
+@Component
+    export default class Workspace extends Vue {
+  @Prop() private msg!: string;
+
+  public async created(): Promise<void> {
+    const { data } = await axios.get('/api/words', {
+      params: {
+        sp: 'fre*',
+        md: 'dp',
+      },
+    });
+
+    data;
+
+    // console.log(data)
+  }
+    }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/ui/STextField.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="s-text-field">
+        <label for="">{{ label }}</label>
+        <input
+                type="text"
+                :placeholder="placeholder"
+        />
+    </div>
+</template>
+
+<script lang="ts">
+    import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+    export default class STextField extends Vue {
+      @Prop()
+      public value!: string | number | null;
+
+      @Prop({ type: String, default: undefined })
+      public label!: string;
+
+      @Prop({ type: String, default: undefined })
+      public placeholder!: string;
+
+      public internalModel: string | number | null = null;
+
+      public created(): void {
+        // console.log(this.value);
+      }
+    }
+</script>
+
+<style lang="scss" scoped>
+.s-text-field {
+
+}
+</style>
