fcff2101a2533850445cdfdda77546568890821a
Mef45 <mefka45@yandex.ru>
2020-08-21 23:55:42 +0500

    feat: реализован UI компонент header (#5)

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,25 @@
 <template>
     <div id="app">
+        <s-header title="Potato Dictionary">
+            <template v-slot:right>
+                Starred Words
+            </template>
+        </s-header>
+
         <Workspace></Workspace>
     </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator'
-import Workspace from './components/Workspace.vue'
+import { Component, Vue } from 'vue-property-decorator';
+
+import Workspace from './components/Workspace.vue';
+import SHeader from './components/ui/SHeader.vue';
 
 @Component({
   components: {
     Workspace,
+    SHeader,
   },
 })
 export default class App extends Vue {}
