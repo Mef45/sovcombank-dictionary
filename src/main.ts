2c48b5c371798c3fe7c3cb0b9cbbeb676dee495d
Mef45 <mefka45@yandex.ru>
2020-08-23 00:51:17 +0500

    feat: реализован flexbox layout

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import store from './store'
 
+import './styles/index.scss'
+
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faSearch } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
