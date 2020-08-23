7e52124b24196d975abde91c0fba0b61e356f63c
Mef45 <mefka45@yandex.ru>
2020-08-23 18:58:46 +0500

    feat: реализованы компоненты list и list-item (#9)

---
+++ src/components/ui/SListItem.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="s-list-item">
+        <div class="s-list-item__text">
+            <slot></slot>
+        </div>
+
+        <div class="s-list-item__icon">
+            <font-awesome-icon icon="chevron-down"></font-awesome-icon>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class SListItem extends Vue {
+
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
