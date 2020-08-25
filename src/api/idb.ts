a4ff18aa3d63f5cde9c97471edfc2a6ba39c9285
mef45 <mefka45@yandex.ru>
2020-08-25 23:47:15 +0500

    feat: реализовал хранение закладов в indexed db

---
+++ src/api/idb.ts
@@ -0,0 +1,79 @@
+import { Bookmark } from '@/store/bookmarks';
+import { Word } from '@/store/dictionary';
+
+const DB_NAME = 'dictionary-db';
+const STORAGE_NAME = 'bookmarks';
+
+const DB_VERSION = 1;
+
+let DB: IDBDatabase;
+
+export default {
+    async getDB(): Promise<IDBDatabase> {
+        return new Promise((res, rej) => {
+            if (DB) return res(DB);
+
+            const request = window.indexedDB.open(DB_NAME, DB_VERSION);
+
+            request.onerror = (e): void => {
+                rej(`Error ${e}`)
+            };
+
+            request.onsuccess = (e: any): void => {
+                DB = e.target.result;
+                res(DB);
+            };
+
+            request.onupgradeneeded = (e: any): void => {
+                const db = e.target.result;
+                db.createObjectStore(STORAGE_NAME, { autoIncrement: true, keyPath: 'id' });
+            }
+        });
+    },
+    async addBookmark(word: Word): Promise<IDBDatabase> {
+        const db = await this.getDB();
+
+        return new Promise(res => {
+            const transaction = db.transaction([STORAGE_NAME], 'readwrite');
+            transaction.oncomplete = (): void => {
+                res();
+            };
+
+            const store = transaction.objectStore(STORAGE_NAME);
+            store.put(word);
+        });
+    },
+    async removeBookmark(word: Bookmark): Promise<IDBDatabase> {
+        const db = await this.getDB();
+
+        return new Promise(res => {
+            const transaction = db.transaction([STORAGE_NAME], 'readwrite');
+            transaction.oncomplete = (): void => {
+                res();
+            };
+
+            const store = transaction.objectStore(STORAGE_NAME);
+            store.delete(word.id);
+        })
+    },
+    async getBookmarks(): Promise<IDBDatabase> {
+        const db = await this.getDB();
+        return new Promise(res => {
+            const transaction = db.transaction([STORAGE_NAME], 'readonly');
+            transaction.oncomplete = (): void => {
+                res(bookmarks);
+            };
+
+            const store = transaction.objectStore(STORAGE_NAME);
+            const bookmarks: any = [];
+
+            store.openCursor().onsuccess = (e: any): void => {
+                const cursor = e.target.result;
+                if (cursor) {
+                    bookmarks.push(cursor.value);
+                    cursor.continue();
+                }
+            }
+        });
+    },
+}
---
+++ src/components/Bookmarks.vue
@@ -0,0 +1,121 @@
+<template>
+    <div class="bookmarks">
+        <div class="container">
+            <div class="row">
+                <div class="column col-3">
+                    <s-text-field
+                            v-model="searchCondition"
+                            label="Поиск"
+                            placeholder="Введите значение"
+                            icon="search"
+                    ></s-text-field>
+                </div>
+
+                <div class="column col-9">
+                    <s-list>
+                        <s-list-item
+                                v-for="(bookmark, index) in bookmarks"
+                                :key="`bookmark-${index}`"
+                        >
+                            <template v-slot:prepend-icon>
+                                <font-awesome-icon
+                                        icon="grip-lines"
+                                ></font-awesome-icon>
+                            </template>
+
+                            <span class="dictionary-word">
+                                {{ bookmark.word }}
+                            </span>
+
+                            <span class="dictionary-part">
+                                {{ bookmark.mainPart }}
+                            </span>
+
+                            <span class="dictionary-definition">
+                                {{ bookmark.mainDefinition }}
+                            </span>
+
+                            <template v-slot:append-icon>
+                                <font-awesome-icon
+                                        :icon="['fas', 'bookmark']"
+                                        @click="removeBookmark($event, bookmark)"
+                                ></font-awesome-icon>
+                            </template>
+
+                            <template v-slot:content>
+                                <!-- затрудняюсь сказать, насколько эти произношения имеют отношение к реальности,
+                              но выбирать не приходится-->
+                                <span style="font-style: italic">/{{ bookmark.pronunciation }}/</span>
+                                <template v-for="(defs, part) in bookmark.definitions">
+                                    <div :key="`${part}-${index}`">
+                                        <span style="font-style: italic">{{ part }}</span>
+                                        <p
+                                                v-for="(def, defIndex) in defs"
+                                                :key="defIndex"
+                                                style="margin-left: 10px; padding-right: 10px;"
+                                        >
+                                            {{ defIndex + 1 }}. {{ def }}
+                                        </p>
+                                    </div>
+                                </template>
+                            </template>
+                        </s-list-item>
+                    </s-list>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+    import { Component, Vue } from 'vue-property-decorator';
+    import { namespace } from 'vuex-class';
+
+    import STextField from '@/components/ui/STextField.vue';
+    import SList from '@/components/ui/SList.vue';
+    import SListItem from '@/components/ui/SListItem.vue';
+
+    import { Bookmark } from '@/store/bookmarks';
+
+    const bookmarks = namespace('bookmarks');
+
+    @Component({
+        components: {
+          STextField,
+          SList,
+          SListItem,
+        },
+    })
+    export default class Bookmarks extends Vue {
+        @bookmarks.Action('getBookmarks')
+        public getBookmarks!: () => Promise<void>;
+
+        @bookmarks.Getter('bookmarks')
+        public bookmarks!: Bookmarks[];
+
+        public searchCondition: string | null = null;
+
+        public async created(): Promise<void> {
+            await this.getBookmarks();
+        }
+
+        public removeBookmark(event: MouseEvent, bookmark: Bookmark): void {
+          event.preventDefault();
+          event.stopPropagation();
+
+          this.getBookmarks();
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+  .dictionary-word {
+    font-weight: bold;
+    margin-right: 40px;
+  }
+
+  .dictionary-part {
+    font-style: italic;
+    margin-right: 40px;
+  }
+</style>
