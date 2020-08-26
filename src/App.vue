837eec2af528ae36387bb29e85e2159ca01bf5e0
mef45 <mefka45@yandex.ru>
2020-08-26 20:01:07 +0500

    codestyle

--- src/App.vue
+++ src/App.vue
@@ -47,43 +47,43 @@
 </script>
 
 <style lang="scss">
-.s-btn {
-    cursor: pointer;
-    height: 30px;
-    padding: 0 10px;
-    border-radius: 4px;
-    position: relative;
-    display: inline-flex;
-    flex: 0 0 auto;
-    align-items: center;
-    user-select: none;
-
-    &:hover:before {
-        opacity: .05;
-    }
-
-    &:before {
-        content: "";
-        border-radius: inherit;
-        background-color: #000000;
-        opacity: 0;
-        pointer-events: none;
-        transition: opacity .2s cubic-bezier(.35, 0, .5, 1);
-        position: absolute;
-        top: 0;
-        left: 0;
-        right: 0;
-        bottom: 0;
-    }
-
-    &__content {
+    .s-btn {
+        cursor: pointer;
+        height: 30px;
+        padding: 0 10px;
+        border-radius: 4px;
         position: relative;
-        font-size: 1rem;
-    }
-}
+        display: inline-flex;
+        flex: 0 0 auto;
+        align-items: center;
+        user-select: none;
 
-.s-btn--active:before,
-.s-btn--active:hover:before {
-    opacity: .1;
-}
+        &:hover:before {
+            opacity: .05;
+        }
+
+        &:before {
+            content: "";
+            border-radius: inherit;
+            background-color: #000000;
+            opacity: 0;
+            pointer-events: none;
+            transition: opacity .2s cubic-bezier(.35, 0, .5, 1);
+            position: absolute;
+            top: 0;
+            left: 0;
+            right: 0;
+            bottom: 0;
+        }
+
+        &__content {
+            position: relative;
+            font-size: 1rem;
+        }
+    }
+
+    .s-btn--active:before,
+    .s-btn--active:hover:before {
+        opacity: .1;
+    }
 </style>
---
+++ src/components/ui/SCheckbox.vue
@@ -0,0 +1,15 @@
+<template>
+    <span></span>
+</template>
+
+<script lang="ts">
+    import { Component, Vue } from 'vue-property-decorator';
+
+    @Component
+    export default class SCheckbox extends Vue {
+
+    }
+</script>
+
+<style scoped>
+</style>
